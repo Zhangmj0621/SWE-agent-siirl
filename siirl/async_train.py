26d84e7c4257125a76f3a52388db12681c01dd31
GaryHu-zzz <984069733@qq.com>
2025-12-10 06:56:25 +0000

    initial(config): initial config class

---
+++ siirl/async_train.py
@@ -0,0 +1,115 @@
+# Copyright 2025, Shanghai Innovation Institute. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
+import time
+import ray
+
+
+from siirl.params import SiiRLArguments, log_dict_formatted, parse_config
+from siirl.utils.logger.logging_utils import set_basic_config
+from siirl.data_coordinator.data_buffer import init_data_coordinator
+from siirl.worker.resouce_pool import create_placement_groups
+# --- Constants ---
+RAY_RUNTIME_ENV_VARS = {
+    "TOKENIZERS_PARALLELISM": "true",
+    "NCCL_DEBUG": "WARN",
+    "VLLM_LOGGING_LEVEL": "WARN",
+}
+
+MAIN_RUNNER_CPU_RESERVATION = 5
+
+
+@ray.remote(num_cpus=MAIN_RUNNER_CPU_RESERVATION)
+class MainRunner:
+    """
+    A Ray actor responsible for orchestrating the entire RL training workflow.
+
+    This actor handles loading configurations, scheduling task graphs, initializing
+    process groups, and launching the distributed Ray trainers. Isolating this
+    orchestration logic in a dedicated actor ensures the main process remains clean
+    and that the setup process is managed within the Ray cluster.
+    """
+
+    def run(self, siirl_args: SiiRLArguments) -> None:
+        """
+        Executes the main training workflow.
+
+        Args:
+            siirl_args: A SiiRLArguments object containing all parsed configurations.
+        """
+        set_basic_config()
+        from loguru import logger
+
+        logger.info("MainRunner started. Beginning workflow setup...")
+        start_time = time.time()
+
+        # 1. Init DataBuffer
+        logger.info(f"Initializing DataCoordinator with {siirl_args.trainer.nnodes} distributed DataBuffers...")
+        # In the new architecture, the number of buffers is typically the number of nodes.
+        # We pass force_local=False to enable distributed deployment.
+        data_coordinator_handle = init_data_coordinator(
+            num_buffers=siirl_args.trainer.nnodes, ppo_mini_batch_size = siirl_args.actor_rollout_ref.actor.ppo_mini_batch_size,
+            world_size=siirl_args.trainer.nnodes * siirl_args.trainer.n_gpus_per_node
+        )
+
+        # 2. initialize pg
+        pgs = create_placement_groups(siirl_args)
+
+        # 3. Initialize rollout worker
+        
+        # 4. Initialize Actor worker
+        
+        # 5. start rollout and actor worker
+
+
+
+
+def main() -> None:
+    """
+    Main entry point for launching the PPO DAG training job.
+
+    This function initializes Ray, parses configurations using Hydra, and
+    starts the MainRunner actor to orchestrate the distributed training workflow.
+
+    Args:
+        siirl_config: The configuration object provided by Hydra.
+    """
+    # Import logger locally to avoid Ray serialization issues
+    from loguru import logger
+
+    start_time = time.time()
+
+    # Initialize Ray cluster if not already running
+    if not ray.is_initialized():
+        logger.info("Initializing local Ray cluster...")
+        ray.init(runtime_env={"env_vars": RAY_RUNTIME_ENV_VARS}, num_cpus=None)
+    logger.success(f"Ray is initialized. Time cost: {(time.time() - start_time) * 1000:.2f} ms")
+
+    # Parse the complete configuration into a structured object
+    siirl_args = parse_config()
+    log_dict_formatted(siirl_args.to_dict(), "SiiRLArguments")
+
+    # Launch the main orchestration actor and wait for it to complete.
+    logger.info("Starting MainRunner actor to orchestrate the job.")
+    runner = MainRunner.remote()
+    # This is a blocking call that waits for the remote `run` method to finish.
+    ray.get(runner.run.remote(siirl_args))
+
+    logger.success("MainRunner has completed its execution. Shutting down.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ siirl/engine/rollout/sglang_engine.py
@@ -0,0 +1,19 @@
+import asyncio
+import io
+import os
+
+from PIL import Image
+import requests
+import sglang as sgl
+
+from sglang.srt.conversation import chat_templates
+from sglang.test.test_utils import is_in_ci
+from sglang.utils import async_stream_and_merge, stream_and_merge
+
+
+
+
+class SglangEngine:
+    def __init__(self, ):
+        
+        
